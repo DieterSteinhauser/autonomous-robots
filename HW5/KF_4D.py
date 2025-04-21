4b2f120d2914ececeba36414e80302683552d2fd
DieterSteinhauser <dieter.steinhauser@outlook.com>
2025-04-21 18:49:49 -0400

    Completed HW5

--- HW5/KF_4D.py
+++ HW5/KF_4D.py
@@ -6,20 +6,92 @@ University Of Florida
 import numpy as np
 
 class KF_4D(object):
-    def __init__(self, dt, u_x, u_y, std_acc, x_std_meas, y_std_meas):
+    def __init__(self, dt, u_x, u_y, u_w, u_h, std_acc, x_std_meas, y_std_meas, w_std_meas, h_std_meas):
         """
-            You will define your control variables
+        dt: sampling time (time for 1 cycle)
+        u_x: acceleration in x-direction
+        u_y: acceleration in y-direction
+        u_w: acceleration in w-direction
+        u_h: acceleration in h-direction
+        std_acc: process noise magnitude
+        x_std_meas: standard deviation of the measurement in x-direction
+        y_std_meas: standard deviation of the measurement in y-direction
+        w_std_meas: standard deviation of the measurement in w-direction
+        h_std_meas: standard deviation of the measurement in h-direction
         """
+        self.dt = dt # sampling time
+        self.u = np.matrix([[u_x],[u_y],[u_w],[u_h]]) # control input variables
+        self.x = np.matrix([[0], [0], [0], [0], [0], [0], [0], [0]]) # intial State
         
-
-
+        # State Transition Matrix A
+        self.A = np.matrix([[1, 0, 0, 0, self.dt, 0, 0, 0],
+                            [0, 1, 0, 0, 0, self.dt, 0, 0],
+                            [0, 0, 1, 0, 0, 0, self.dt, 0],
+                            [0, 0, 0, 1, 0, 0, 0, self.dt],
+                            [0, 0, 0, 0, 1, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 1, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 1, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 1]])
+        
+        # Control Input Matrix B
+        self.B = np.matrix([[(self.dt**2)/2, 0, 0, 0],
+                            [0,(self.dt**2)/2, 0, 0],
+                            [0,0,(self.dt**2)/2, 0],
+                            [0,0,0,(self.dt**2)/2],
+                            [self.dt,0,0,0],
+                            [0,self.dt,0,0],
+                            [0,0,self.dt,0],
+                            [0,0,0,self.dt]])
+        
+        # Measurement Mapping Matrix
+        self.H = np.matrix([[1, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 1, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 1, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 1, 0, 0, 0, 0]])
+        
+        # Initial Process Noise Covariance
+        self.Q = np.matrix([[(self.dt**4)/4, 0, 0, 0, (self.dt**3)/2, 0, 0, 0],
+                            [0, (self.dt**4)/4, 0, 0, 0, (self.dt**3)/2, 0, 0],
+                            [0, 0, (self.dt**4)/4, 0, 0, 0, (self.dt**3)/2, 0],
+                            [0, 0, 0, (self.dt**4)/4, 0, 0, 0, (self.dt**3)/2],
+                            [(self.dt**3)/2, 0, 0, 0, self.dt**2, 0, 0, 0],
+                            [0, (self.dt**3)/2, 0, 0, 0, self.dt**2, 0, 0],
+                            [0, 0, (self.dt**3)/2, 0, 0, 0, self.dt**2, 0],
+                            [0, 0, 0, (self.dt**3)/2, 0, 0, 0, self.dt**2]]) * std_acc**2
+        
+        #Initial Measurement Noise Covariance
+        self.R = np.matrix([[x_std_meas**2, 0, 0, 0],
+                            [0, y_std_meas**2, 0, 0],
+                            [0, 0, w_std_meas**2, 0],
+                            [0, 0, 0, h_std_meas**2]])
+        
+        #Initial Covariance Matrix
+        self.P = np.eye(self.A.shape[1])
+        
     def predict(self):
         ## complete this function
         
+        # Update time state (self.x): x_k =Ax_(k-1) + Bu_k
+        self.x = self.A * self.x + self.B * self.u
+        
+        # Calculate error covariance (self.P): P= A*P*A' + Q
+        self.P = self.A * self.P * self.A.T + self.Q
+        
         return self.x[0:4]
-
-
+    
     def update(self, z):
         ## complete this function
+        
+        # Calculate S = H*P*H'+R
+        S = self.H * self.P * self.H.T + self.R
+        
+        # Calculate the Kalman Gain K = P * H'* inv(S)
+        K = self.P * self.H.T * np.linalg.inv(S)
+        
+        # Update self.x
+        self.x = self.x + K *(z - self.H * self.x)
+        
+        # Update error covariance matrix self.P
+        self.P = (np.eye(self.A.shape[1]) - K * self.H) * self.P
         
         return self.x[0:4]
--- HW5/boxTracker.py
+++ HW5/boxTracker.py
@@ -18,11 +18,36 @@ def boxDetector(image):
     ret, img_thresh = cv2.threshold(img_edges, 254, 255, cv2.THRESH_BINARY) # convert to binary images
     contours, _ = cv2.findContours(img_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # get object contours
 
-    """
-    Your code for returning bounding box (rectangle) instead of the circle
-    """
+    # Inserted code here
+    
+    # initialize the boxes and centers lists
+    boxes = []
+    centers=[]
+    
+    min_radius, max_radius = 3, 30
+    
+    # if there are contours in the image, proceed with bounding box detection
+    if contours:
+        
+        # loop through each contour in the image
+        for contour in contours:
+            
+            # Find the minimum enclosing circle around the contour
+            (x, y), radius = cv2.minEnclosingCircle(contour)
+            radius = int(radius)
+            
+            # if the radius is within the specified range
+            if (radius > min_radius) and (radius < max_radius): # Take only the valid circle(s)
+                
+                # add the center of the circle to the centers list
+                centers.append(np.array([[x], [y]]))
+                
+                # add the bounding box around the circle to the boxes list
+                x, y, w, h = cv2.boundingRect(contour)
+                boxes.append((x, y, w, h))
+    
     cv2.imshow('contours', img_thresh)
-    return boxes
+    return boxes, centers
 
 
 
@@ -32,17 +57,49 @@ VideoCap = cv2.VideoCapture('data/rBall.avi')
 """
 # Create Kalman Filter object KF_4D
 """
-filter = KF_4D("You will call the function")
+dt = 0.1 # Sampling time
+u_x = 1  # Acceleration in x-direction
+u_y = 1  # Acceleration in y-direction
+u_w = 1  # Acceleration in w-direction
+u_h = 1  # Acceleration in h-direction
+std_acc = 1 # Process noise magnitude
+x_std_meas = 0.1  # Standard deviation of the measurement in x-direction
+y_std_meas = 0.1  # Standard deviation of the measurement in y-direction
+w_std_meas = 0.1  # Standard deviation of the measurement in w-direction
+h_std_meas = 0.1  # Standard deviation of the measurement in h-direction
+filter = KF_4D(dt, u_x, u_y, u_w, u_h, std_acc, x_std_meas, y_std_meas, w_std_meas, h_std_meas)
 
 # Track bounding box (predict + update) using KF_4D
 while(True):
     ret, frame = VideoCap.read() # Read frame
-    box = boxDetector(frame) # Detect object
-    
-    """
-       Your code
-    """
+    boxes, centers = boxDetector(frame) # Detect object
 
+
+    if boxes:
+        x, y, w, h = boxes[0]
+        measurement = np.matrix([[x], [y], [w], [h]])
+        
+        # Draw measured circle and box in Yellow
+        cv2.circle(frame, (int(centers[0][0]), int(centers[0][1])), 10, (0, 191, 255), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 191, 255), 2)
+        cv2.putText(frame, "Measured Position", (x + 15, y - 15), 0, 0.5, (0, 191, 255), 2)
+        
+        # Draw predicted box in Blue
+        xp, yp, wp, hp = filter.predict().A.flatten().astype(int)
+
+        # image = cv2.rectangle(image, start_point, end_point, color, thickness)
+        #cv2.rectangle(frame, (xp - int(wp/2), yp - int(hp/2)), (xp + int(wp/2), yp + int(hp/2)), (255, 0, 0), 2)
+        cv2.rectangle(frame, (xp, yp), (xp + wp, yp + hp), (255, 0, 0), 2)
+        cv2.putText(frame, "Predicted Position", (xp + 15, yp - 15), 0, 0.5, (255, 0, 0), 2)
+        
+        # Draw updated estimation box in Red
+        xu, yu, wu, hu = filter.update(measurement).A.flatten().astype(int)
+        
+        # image = cv2.rectangle(image, start_point, end_point, color, thickness)
+        #cv2.rectangle(frame, (xu - int(wu/2), yu - int(hu/2)), (xu + int(wu/2), yu + int(hu/2)), (0, 0, 255), 2)
+        cv2.rectangle(frame, (xu, yu), (xu + wu, yu + hu), (0, 0, 255), 2)
+        cv2.putText(frame, "Estimated Position", (xu + 15, yu - 15), 0, 0.5, (0, 0, 255), 2)
+  
     # Show output and wait for keypress
     cv2.imshow('image', frame)
     if cv2.waitKey(2) & 0xFF == ord('q'):
